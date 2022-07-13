67fa44368543df355fab08428867d39972b878cf
halitov-r-k <halitov.r.k@gmail.com>
2022-07-13 21:48:12 +0500

    2022-07-13

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+       Scanner scanner =new Scanner(System.in);
+       double sum = 0;
+           while (scanner.hasNext()) {
+           if (scanner.hasNextDouble()) {
+               sum += Double.parseDouble(scanner.next());
+           } else {
+               scanner.next();
+           }
+       }
+        System.out.printf("%.6f",sum);
+    }
+}
